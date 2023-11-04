7d6baab624fe3f976caf7b90b5597770b3ddf198
Sujan Karki <83892476+Apocalypse526@users.noreply.github.com>
2023-11-04 17:17:23 +0545

    created themeBTN and Card Btn, added toggle fxnality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
 import { ThemeProvider } from "./context/theme"
+import ThemeBtn from "./components/ThemeBtn";
+import Card from './components/Card';
 
 function App() {
   const[themeMode, setThemeMode] = useState('light');
@@ -22,10 +24,10 @@ function App() {
      <div className="flex flex-wrap min-h-screen items-center">
     <div className="w-full">
       <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-        {/* ThemeBTN */}
+        <ThemeBtn/>
       </div>
       <div className="w-full max-w-sm mx-auto">
-        {/* Card */}
+        <Card/>
       </div>
     </div>
    </div>
